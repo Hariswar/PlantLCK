eb80e64ba31fd812f57725ad92e75bcbc3732ccc
Hariswar <bkhariswar@gmail.com>
2026-04-27 12:21:27 -0500

    pulling the data from the backend api

--- nlp/preprocessing/data.py
+++ nlp/preprocessing/data.py
@@ -1,4 +1,10 @@
-# just created a file for data preprocessing. 
-import pandas as pad 
-import pathlib
-import spacy
+import pandas as pd
+from pathlib import Path
+import requests
+
+
+url = "http://localhost:8000"
+cyberpunk_2027ID = 1091500 
+limit_of_the_game_reviews = None
+
+
