357a6a0436fb499f11583ee9b83c50a002f77887
Hariswar <bkhariswar@gmail.com>
2026-05-06 21:29:39 -0500

    done basic preprocessing steps

--- nlp/preprocessing/preprocessing.py
+++ nlp/preprocessing/preprocessing.py
@@ -8,10 +8,14 @@ nlp = spacy.load("en_core_web_sm")
 data_format = 'review_text'
 
 def cleaning_reviews(data):
+  punctuations = r'[^a-zA-Z0-9\s]'
+  symbol_check = r'@[A-Za-z0-9_]+'
   data = data.dropna(subset=[data_format])
   data[data_format] = data[data_format].str.lower()
   data = data.drop_duplicates(subset=[data_format])
   data[data_format] = data[data_format].str.strip()
+  data[data_format] = data[data_format].str.replace(symbol_check, '', regex=True)
+  data[data_format] = data[data_format].str.replace(punctuations, '', regex=True)
   data = data[data[data_format] != '']
   return data
 
